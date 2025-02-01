3c8c45e6a0299ebde7731eadb57b3e344efb30c1
antiNT2 <32984684+antiNT2@users.noreply.github.com>
2025-02-01 10:27:33 -0500

    test attempt #2

--- sonar-scanner-engine/src/it/java/org/sonar/scanner/mediumtest/bootstrap/BootstrapMediumIT.java
+++ sonar-scanner-engine/src/it/java/org/sonar/scanner/mediumtest/bootstrap/BootstrapMediumIT.java
@@ -178,7 +178,7 @@ class BootstrapMediumIT {
       .addProperty(SONAR_PROJECT_KEY, PROJECT_KEY)
       .addProperty(SONAR_PROJECT_BASE_DIR, baseDir.toString()));
 
-    assertTrue("This should be true", true);
+    assertThat(true).isTrue();
     //assertThat(logTester.logs()).contains("SonarScanner Engine completed successfully");
   }
 
